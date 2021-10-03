749faa2bec74f7c6691e8f9f9b815217e26a0716
asamuzaK (Kazz) <asamuzaK@users.noreply.github.com>
2021-10-03 15:24:07 +0900

    Switch to ES modules (#6)

--- index.js
+++ index.js
@@ -4,15 +4,8 @@
  * @see {@link http://semver.org/ Semantic Versioning}
  * @see {@link https://github.com/semver/semver/ Specification}
  */
-'use strict';
-/* api */
-const {
-  compareSemVer, isValidSemVer, parseSemVer, promises
-} = require('./modules/semver');
 
-module.exports = {
-  compareSemVer,
-  isValidSemVer,
-  parseSemVer,
-  promises
-};
+/* api */
+export {
+  compareSemVer, isValidSemVer, parseSemVer, promises
+} from './modules/semver.js';
--- test/common.test.js
+++ test/common.test.js
@@ -1,30 +1,12 @@
-'use strict';
 /* api */
-const {
-  escapeChar, getType, isString, logErr, logMsg, logWarn, parseStringifiedInt,
-  stringifyPositiveInt, stripHtmlTags, throwErr
-} = require('../modules/common');
-const { assert } = require('chai');
-const { describe, it } = require('mocha');
-const sinon = require('sinon');
+import { assert } from 'chai';
+import { describe, it } from 'mocha';
+import sinon from 'sinon';
 
-describe('escapeChar', () => {
-  it('should get escaped string', () => {
-    const c = 'abc';
-    const re = /(b)/ig;
-    assert.strictEqual(escapeChar(c, re), 'a\\bc');
-  });
-
-  it('should get null if string is not given', () => {
-    const re = /(b)/ig;
-    assert.isNull(escapeChar(1, re));
-  });
-
-  it('should get null if regexp is not given', () => {
-    const c = 'abc';
-    assert.isNull(escapeChar(c));
-  });
-});
+/* test */
+import {
+  getType, isString, logErr, logMsg, logWarn, throwErr
+} from '../modules/common.js';
 
 describe('getType', () => {
   it('should get Undefined', () => {
@@ -111,108 +93,6 @@ describe('logWarn', () => {
     assert.isTrue(calledOnce);
     assert.strictEqual(warnMsg, msg);
     assert.isFalse(res);
-  });
-});
-
-describe('stringifyPositiveInt', () => {
-  it('should get string', () => {
-    assert.strictEqual(stringifyPositiveInt(1), '1');
-  });
-
-  it('should get null if given argument is not positive integer', () => {
-    assert.isNull(stringifyPositiveInt());
-  });
-
-  it('should get null if 0 is given', () => {
-    assert.isNull(stringifyPositiveInt(0));
-  });
-
-  it('should treat 0 as positive integer if second argument is true', () => {
-    assert.strictEqual(stringifyPositiveInt(0, true), '0');
-  });
-});
-
-describe('parseStringifiedInt', () => {
-  it('should be function', () => {
-    assert.typeOf(parseStringifiedInt, 'function');
-  });
-
-  it('should throw if argument is not string', () => {
-    assert.throws(() => parseStringifiedInt());
-  });
-
-  it('should throw if argument contains non-numeric string', () => {
-    assert.throws(() => parseStringifiedInt('1a'));
-  });
-
-  it('should throw if argument contains non-numeric string', () => {
-    assert.throws(() => parseStringifiedInt('1.0'));
-  });
-
-  it('should be integer', () => {
-    assert.strictEqual(parseStringifiedInt('0'), 0);
-  });
-
-  it('should be integer', () => {
-    assert.strictEqual(parseStringifiedInt('1'), 1);
-  });
-
-  it('should be integer', () => {
-    assert.strictEqual(parseStringifiedInt('-1'), -1);
-  });
-
-  it('should be integer', () => {
-    assert.strictEqual(parseStringifiedInt('10'), 10);
-  });
-
-  it('should throw if leading zero', () => {
-    assert.throws(() => parseStringifiedInt('01'));
-  });
-
-  it('should be integer if leading zero but second arg is true', () => {
-    assert.strictEqual(parseStringifiedInt('01', true), 1);
-  });
-
-  it('should throw if leading zero', () => {
-    assert.throws(() => parseStringifiedInt('010'));
-  });
-
-  it('should be integer if leading zero but second arg is true', () => {
-    assert.strictEqual(parseStringifiedInt('010', true), 10);
-  });
-
-  it('should throw if negative int leading zero', () => {
-    assert.throws(() => parseStringifiedInt('-01'));
-  });
-
-  it(
-    'should be integer if negative int leading zero but second arg is true',
-    () => {
-      assert.strictEqual(parseStringifiedInt('-01', true), -1);
-    }
-  );
-
-  it('should throw if negative int leading zero', () => {
-    assert.throws(() => parseStringifiedInt('-010'));
-  });
-
-  it(
-    'should be integer if negative int leading zero but second arg is true',
-    () => {
-      assert.strictEqual(parseStringifiedInt('-010', true), -10);
-    }
-  );
-});
-
-describe('stripHtmlTags', () => {
-  it('should strip HTML tags', () => {
-    const p = '<p>test</p>';
-    assert.strictEqual(stripHtmlTags(p), 'test\n');
-  });
-
-  it('should decode HTML entities', () => {
-    const p = '&amp;&lt;&gt;&quot;';
-    assert.strictEqual(stripHtmlTags(p), '&<>"');
   });
 });
 
