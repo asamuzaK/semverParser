4bbee923f508e401c4a456ae8df05699c0a6689f
asamuzaK (Kazz) <kazz@asamuzak.jp>
2023-02-12 14:06:27 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 /**
- * index.js
+ * SemVer Parser
  *
- * @see {@link http://semver.org/ Semantic Versioning}
- * @see {@link https://github.com/semver/semver/ Specification}
+ * @license MIT
+ * @copyright asamuzaK (Kazz)
+ * @see {@link https://github.com/asamuzaK/semverParser/blob/master/LICENSE}
+ * @see {@link https://semver.org/ Semantic Versioning 2.0.0}
  */
 
-/* api */
 export {
   compareSemVer, isValidSemVer, parseSemVer, promises
 } from './modules/semver.js';
